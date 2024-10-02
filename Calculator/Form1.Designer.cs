2fac7d3fd0599676afd881dd5c5cb30baf211913
VolkKatasi <117032738+VolkKatasi@users.noreply.github.com>
2024-10-02 22:55:46 +0400

    Калькулятор вычетания
    вычетание

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.one = new System.Windows.Forms.Button();
+            this.rovno = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.defect = new System.Windows.Forms.Button();
+            this.point = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // one
+            // 
+            this.one.Location = new System.Drawing.Point(12, 94);
+            this.one.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(35, 35);
+            this.one.TabIndex = 1;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // rovno
+            // 
+            this.rovno.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rovno.Location = new System.Drawing.Point(128, 12);
+            this.rovno.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.rovno.Name = "rovno";
+            this.rovno.Size = new System.Drawing.Size(37, 76);
+            this.rovno.TabIndex = 3;
+            this.rovno.Text = "=";
+            this.rovno.UseVisualStyleBackColor = true;
+            this.rovno.Click += new System.EventHandler(this.rovno_Click);
+            // 
+            // two
+            // 
+            this.two.Location = new System.Drawing.Point(51, 94);
+            this.two.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(35, 35);
+            this.two.TabIndex = 4;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // three
+            // 
+            this.three.Location = new System.Drawing.Point(89, 94);
+            this.three.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(35, 35);
+            this.three.TabIndex = 5;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // four
+            // 
+            this.four.Location = new System.Drawing.Point(12, 53);
+            this.four.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(35, 35);
+            this.four.TabIndex = 6;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // five
+            // 
+            this.five.Location = new System.Drawing.Point(51, 53);
+            this.five.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(35, 35);
+            this.five.TabIndex = 7;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // six
+            // 
+            this.six.Location = new System.Drawing.Point(89, 53);
+            this.six.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(35, 35);
+            this.six.TabIndex = 8;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // seven
+            // 
+            this.seven.Location = new System.Drawing.Point(11, 12);
+            this.seven.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(35, 35);
+            this.seven.TabIndex = 9;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // eight
+            // 
+            this.eight.Location = new System.Drawing.Point(51, 12);
+            this.eight.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(35, 35);
+            this.eight.TabIndex = 10;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // nine
+            // 
+            this.nine.Location = new System.Drawing.Point(89, 12);
+            this.nine.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(35, 35);
+            this.nine.TabIndex = 11;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // zero
+            // 
+            this.zero.Location = new System.Drawing.Point(11, 135);
+            this.zero.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(71, 35);
+            this.zero.TabIndex = 12;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // defect
+            // 
+            this.defect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.defect.Location = new System.Drawing.Point(128, 94);
+            this.defect.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.defect.Name = "defect";
+            this.defect.Size = new System.Drawing.Size(35, 35);
+            this.defect.TabIndex = 15;
+            this.defect.Text = "-";
+            this.defect.UseVisualStyleBackColor = true;
+            this.defect.Click += new System.EventHandler(this.defect_Click);
+            // 
+            // point
+            // 
+            this.point.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.point.Location = new System.Drawing.Point(87, 135);
+            this.point.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(35, 35);
+            this.point.TabIndex = 16;
+            this.point.Text = ",";
+            this.point.UseVisualStyleBackColor = true;
+            this.point.Click += new System.EventHandler(this.point_Click);
+            // 
+            // delete
+            // 
+            this.delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.delete.ForeColor = System.Drawing.Color.Black;
+            this.delete.Location = new System.Drawing.Point(128, 135);
+            this.delete.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(37, 35);
+            this.delete.TabIndex = 19;
+            this.delete.Text = "⌫";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(376, 195);
+            this.textBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(103, 20);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(174, 183);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.defect);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.rovno);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.textBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button rovno;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button defect;
+        private System.Windows.Forms.Button point;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        Form2 form2;
+        public Form1()
+        {
+            InitializeComponent();
+            form2 = new Form2(); // форм 2 
+        }
+
+        public void ChangeColor1(Color newColor)
+        {
+            this.BackColor = newColor;
+        }
+        public void ChangeColor2(Color newColor)
+        {
+            this.BackColor = newColor;
+        }
+
+        public double i;
+        public double num1, num2, num3;
+        public double memory = 0;
+        
+        private double Factorial(double n)
+        {
+            
+            int res = 1;
+            for (int j = 1; j <= n; j++)
+            {
+                res *= j;
+            }
+            return res;
+        }
+
+        //  нажатие цифр //
+        private void zero_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "0")
+            {
+                textBox.Text = "0";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            else
+            {
+                textBox.Text += "0";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            form2.Show(); 
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "0")
+            {
+                textBox.Text = "1";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            else
+            {
+                textBox.Text += "1";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            form2.Show(); 
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "0")
+            {
+                textBox.Text = "2";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            else
+            {
+                textBox.Text += "2";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            form2.Show(); 
+        }
+        private void three_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "0")
+            {
+                textBox.Text = "3";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            else
+            {
+                textBox.Text += "3";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            form2.Show(); 
+        }
+        private void four_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "0")
+            {
+                textBox.Text = "4";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            else
+            {
+                textBox.Text += "4";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            form2.Show(); 
+        }
+        
+        private void five_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "0")
+            {
+                textBox.Text = "5";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            else
+            {
+                textBox.Text += "5";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            form2.Show(); 
+        }
+        private void six_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "0")
+            {
+                textBox.Text = "6";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            else
+            {
+                textBox.Text += "6";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            form2.Show(); 
+        }
+        private void seven_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "0")
+            {
+                textBox.Text = "7";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            else
+            {
+                textBox.Text += "7";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            form2.Show(); 
+        }
+        private void eight_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "0")
+            {
+                textBox.Text = "8";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            else
+            {
+                textBox.Text += "8";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            form2.Show(); 
+        }
+        private void nine_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "0")
+            {
+                textBox.Text = "9";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            else
+            {
+                textBox.Text += "9";
+                form2.SetText(textBox.Text); // Передаем значение в Form2
+            }
+            form2.Show(); 
+        }
+        private void point_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Проверяем, что текст не пустой и последний символ не является запятой
+                if (textBox.Text != "" && !textBox.Text.EndsWith(","))
+                {
+
+                    if (!char.IsDigit(textBox.Text[textBox.Text.Length - 1]))
+                    {
+
+                    }
+                    if (textBox.Text == "0")
+                        textBox.Text = "0,";
+                    else
+                        textBox.Text += ",";
+                }
+                form2.Show();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Не выбрано число. Пожалуйста, введите корректное число.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+       
+        // Арифметические операции //
+        
+        private void defect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Проверяем, что текст не пустой и последний символ не является запятой
+                if (textBox.Text == "0" || textBox.Text == "")
+                    textBox.Text = "0";
+                else
+                {
+                    i = 4;
+                    num1 = Convert.ToDouble(textBox.Text);
+                    textBox.Text = "";
+                    textBox.Focus();
+
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Не выбрано число. Пожалуйста, введите корректное число.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // Others //
+        private void delete_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "0";
+            form2.SetText(textBox.Text);
+        }
+        private void rovno_Click(object sender, EventArgs e)
+        {
+            if (i == 4)
+            {
+                num2 = Double.Parse(textBox.Text);
+                num3 = num1 - num2;
+                textBox.Text = num3.ToString();
+                form2.SetText(textBox.Text);
+            }
+            if (i == 5)
+            {
+                num2 = Double.Parse(textBox.Text);
+                num3 = Math.Pow(num1,num2);
+                form2.SetText(textBox.Text);
+                textBox.Text = num3.ToString();
+            }
+            if (i == 6)
+            {
+                num2 = Double.Parse(textBox.Text);
+                num3 = Math.Pow(num1, (double)1 / num2);
+                textBox.Text = num3.ToString();
+                form2.SetText(textBox.Text);
+            }
+        }
+    }
+}
